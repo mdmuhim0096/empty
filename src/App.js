49bf17afe175b6d010e5e4ca53343cab9bcb1dc5
muhim <muhinking765@gmail.com>
2024-12-15 02:56:07 +0600

    hello

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+
+  return (
+    <>
+     <h1 className="text-center text-9xl text-teal-500 animate-bounce underline decoration-green-500 my-20 capitalize font-bold italic">hello i am react</h1>
+    </>
+  )
+}
+
+export default App;
